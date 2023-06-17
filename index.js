c226b2c0bf7a4b61728c1b2d2861fe3b26494ea7
Roomiik <73971455+Roomiik@users.noreply.github.com>
2023-06-17 23:33:52 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+let Button = document.querySelector(".button")
+let NameS = document.querySelector("#startup-name")
+let founders = document.querySelector("#founders")
+let description = document.querySelector("#descriptione")
+let email = document.querySelector("#email")
+
+const submitForm = () => {
+    alert("Ваша заявка була успішно відправлена менеджерам")
+    // (Names.lenght != 0 && founders.lenght != 0 && description.lenght != 0 && email.lenght != 0) ? alert("Ваша заявка була успішно відправлена менеджерам") : alert("Ви не заповнили повністю форму");
+}
